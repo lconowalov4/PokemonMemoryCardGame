3b06b0f8aa6adf7e835a63e52d311681d1b035b4
lconowalov4 <rw6lfe@gmail.com>
2026-02-16 16:58:04 +0300

    Added card shuffling function

---
+++ main.js
@@ -0,0 +1,38 @@
+let errors = 0;
+let cardList = [
+    "darkness",
+    "double",
+    "fairy",
+    "fighting",
+    "fire",
+    "grass",
+    "lightning",
+    "metal",
+    "psychic",
+    "water"
+]
+
+let cardSet;
+let board = [];
+
+window.onload = function() {
+    shuffleCards();
+    startGame();
+}
+
+function shuffleCards(){
+    cardSet = cardList.concat(cardList); // Two of each card
+    console.log(cardSet);
+
+    for (let i = 0; i < cardSet.length; i++) {
+        let j = Math.floor(Math.random() * cardSet.length) // Get random index
+        let temp = cardSet[i];
+        cardSet[i] = cardSet[j];
+        cardSet[j] = temp;
+    }
+    console.log(cardSet);
+}
+
+function startGame(){
+    
+}
